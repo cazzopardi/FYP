5416929766bcddc3e7850c33e576e1d7949582de
Calvin <calvin.azzopardi.21@um.edu.mt>
2024-04-14 20:32:46 +0200

    [preprocessing] fix: fixed label encoding bug in ssc_ocsvm

--- src/preprocessing/pu_et_al.py
+++ src/preprocessing/pu_et_al.py
@@ -1,17 +1,3 @@
-'''
-convert protocol_type, service and flag to one-hot encoding
-F-test for feature selection
-normlaisation with StandardScaler (-u/variance)
-split into 4 single attack cat subsets and mixed type
-
-DoS
-U2R
-R2L
-probe or scan
-mixed
-
-SSC-OCSVM
-'''
 import os
 
 import numpy as np
@@ -20,6 +6,10 @@ from sklearn.feature_selection import SelectKBest, f_classif, f_regression
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
+# label encoding maps
+category_encoding = {'normal':0, 'DoS':1, 'probe':2, 'r2l':3, 'u2r':4}
+output_encoding = {'category': category_encoding}
+
 def _one_hot_encode(data: pd.DataFrame, feature: str) -> pd.DataFrame:
     levels: np.ndarray =  data[feature].unique()
     for level in levels:
@@ -27,7 +17,7 @@ def _one_hot_encode(data: pd.DataFrame, feature: str) -> pd.DataFrame:
 
     return data
 
-def preprocess(data: pd.DataFrame, target: str='category') -> tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+def preprocess(data: pd.DataFrame, target: str='category') -> dict[str, tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]]:
     # one-hot encoding
     non_numeric_features = ['protocol_type', 'service', 'flag']
     for non_numeric_feature in non_numeric_features:
@@ -39,21 +29,14 @@ def preprocess(data: pd.DataFrame, target: str='category') -> tuple[pd.DataFrame
     cat_list = pd.read_csv(os.path.join('data','NSL-KDD', 'category_list.csv'))
     cat_list = pd.Series(cat_list['Attack Category'].values, index=cat_list['Label'])
     data['category'] = data['class'].map(cat_list)
-
-    # dataset splitting
-    subsets: dict[str, pd.DataFrame] = {}
-    for l in levels:
-        mask: pd.Series = (data['category'] == l) | (data['category'] == 'normal')
-        subsets[l] = data[mask]
-    subsets['mixed'] = pd.concat([s for s in subsets.values()])
     
     # split features and labels
     X: pd.DataFrame = data.drop(columns=['class', 'category'])
-    output_encoding: dict[str, int] = {lbl:enc for enc, lbl in enumerate(np.unique(data['category']))}
-    y: pd.Series = data['category'].map(output_encoding)
+    # output_encoding: dict[str, int] = {lbl:enc for enc, lbl in enumerate(np.unique(data['category']))}
+    y: pd.Series = data['category'].map(output_encoding[target])
 
     # feature selection
-    selector = SelectKBest(score_func=f_classif, k=50)
+    selector = SelectKBest(score_func=f_classif, k=15)
     selector.fit(X, y)
 
     # Get indices of selected features
@@ -61,12 +44,20 @@ def preprocess(data: pd.DataFrame, target: str='category') -> tuple[pd.DataFrame
 
     # Get selected feature names
     X = X.loc[:, feature_mask]
-    print(X.columns)
 
     # standardisation
     scaler = StandardScaler()
-    X = pd.DataFrame(scaler.fit_transform(X))
+    X = pd.DataFrame(scaler.fit_transform(X), index=X.index)
 
+    # split attack types
+    subsets: dict[str, tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]] = {}
+    for l in levels:
+        if l == 0:
+            continue
+        mask: pd.Series = (y == l) | (y == 0)
+        X_train, X_test, y_train, y_test = train_test_split(X[mask], y[mask], test_size=0.2, random_state=42, shuffle=True)
+        subsets[l] = X_train, X_test, y_train, y_test
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=True)
-
-    return X_train, X_test, y_train, y_test
+    subsets['mixed'] = X_train, X_test, y_train, y_test
+    
+    return subsets
