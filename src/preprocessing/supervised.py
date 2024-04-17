65b03da09169e101012589237e83e503b1537efa
Calvin <calvin.azzopardi.21@um.edu.mt>
2024-04-17 14:53:44 +0200

    [preprocessing] feature: added SMOTE for supervised model

--- src/preprocessing/supervised.py
+++ src/preprocessing/supervised.py
@@ -1,5 +1,13 @@
+from functools import partial
+import logging
+import math
+from multiprocessing import Pool
+import random
 import sys
+import os
 from typing import TypeVar
+
+from sklearn.neighbors import NearestNeighbors
 sys.path.append('./src')
 
 import pandas as pd
@@ -13,57 +21,72 @@ from data.loading import load_cic_ids_2018, CIC_IDS_2018
 
 TARGET = 'attack category'
 
-# class SMOTE:
-#     def __init__(self, k):
-#         self.k = k
+class SMOTE:
+    label_col: str = '_label columns'
+    def __init__(self, k, n_jobs:int=-1):
+        self.k = k
+        if n_jobs == -1:
+            self.n_jobs = os.cpu_count()
+        else:
+            self.n_jobs = n_jobs
 
-#     def fit_resample(self, data: pd.DataFrame, amounts: dict[int, int]):
-#         """
-#         data: original dataset
-#         amounts: dict mapping minority class to number of samples to generate
-#         """
-#         acc: List[pd.DataFrame] = []
-#         for minority_class, num_samples in amounts.items():
-#             samples: pd.DataFrame = data[data[TARGET] == minority_class]
-#             synth: np.ndarray = self._fit(samples, math.ceil(num_samples/len(samples))*100)
-#             acc.append(pd.DataFrame(synth, columns=data.columns))
-#         resampled_data: pd.DataFrame = pd.concat([data, *acc])
-#         return resampled_data.sample(frac=1)
+    def fit_resample(self, X: pd.DataFrame, y: np.ndarray, amounts: dict[int, int]) -> tuple[pd.DataFrame, np.ndarray]:
+        """
+        data: original dataset
+        amounts: dict mapping minority class to number of samples to generate
+        """
+        acc: list[pd.DataFrame] = []
+        for minority_class, num_samples in amounts.items():
+            samples: pd.DataFrame = X[y == minority_class]
+            synth: np.ndarray = self._fit(samples, math.ceil(num_samples/len(samples))*100)
+            df = pd.DataFrame(synth, columns=X.columns)
+            df[self.label_col] = minority_class
+            acc.append(df)
+        data: pd.DataFrame = pd.concat([X,pd.Series(y, name=self.label_col, index=X.index)], axis=1)
+        resampled_data: pd.DataFrame = pd.concat([data, *acc], ignore_index=True).sample(frac=1, random_state=42)
+        return resampled_data.drop(columns=[self.label_col]), resampled_data[self.label_col].to_numpy()
 
-#     def _fit(self, minority_samples: pd.DataFrame, N: int) -> np.ndarray:
-#         """
-#         T: Number of minority class samples
-#         N: Amount of SMOTE (as percentage) Likely need to be integral multiple of 100
-#         k: number of nearest neighbours
-#         """
-#         T: int = len(minority_samples)
-#         if N < 100:
-#             minority_samples.shuffle()
-#             T = (N/100)*T
-#             N = 100
-#             logging.warn("SMOTE received amount lower than 100% this may influence the shape of the resulting resampled data")
-#         N = int(N/100)
-#         # numattrs = len(data.columns)  # num attributes
-#         self.newindex: int = 0 # tracks number of synthetic samples
-#         synthetic: np.ndarray = np.empty((N, len(minority_samples.columns)))  # new samples
-#         knn: NearestNeighbors = NearestNeighbors(n_neighbors=self.k)
-#         knn.fit(minority_samples)
-#         for i in range(T):
-#             nnarray: np.ndarray = knn.kneighbors([minority_samples.iloc[i]], return_distance=False)[0]
-#             self.populate(minority_samples, synthetic, N, i, nnarray)
-#         return synthetic
+    def _fit(self, minority_samples: pd.DataFrame, N: int) -> np.ndarray:
+        """
+        T: Number of minority class samples
+        N: Amount of SMOTE (as percentage) Likely need to be integral multiple of 100
+        k: number of nearest neighbours
+        """
+        T: int = len(minority_samples)
+        if N < 100:
+            minority_samples.sample(frac=1)  # shuffle
+            if N % 100 != 0:
+                raise ValueError("N must be integral multiple of 100")
+            T = int((N/100)*T)
+            N = 100
+            logging.warn("SMOTE received amount lower than 100% this may influence the shape of the resulting resampled data")
+        N = int(N/100)
+        # numattrs = len(data.columns)  # num attributes
+        # self.newindex: int = 0 # tracks number of synthetic samples
+        # synthetic: np.ndarray = np.empty((N*T, len(minority_samples.columns)))  # new samples  # TODO: verify N*T
+        knn: NearestNeighbors = NearestNeighbors(n_neighbors=self.k, n_jobs=self.n_jobs)
+        knn.fit(minority_samples)
+        neighbours = knn.kneighbors(minority_samples, return_distance=False)
+        # for i in range(T):  # TODO: parallelise
+        #     nnarray: np.ndarray = neighbours[i]
+        #     self.populate(nnarray, i, self.k, N, minority_samples)
+        with Pool(processes=self.n_jobs) as p:
+            f = partial(populate, k=self.k, N=N, minority_samples=minority_samples)
+            synthetic: np.ndarray = np.vstack(p.starmap(f, zip(neighbours, range(T))))
+        return synthetic
 
-#     def populate(self, minority_samples, synthetic, N, i, nnarray):
-#         """
-#         Generates a sample
-#         """
-#         for _ in range(N):
-#             nn: int = random.randint(0, self.k-1)
-#             for col_index, col in enumerate(minority_samples.columns):
-#                 dif: float = minority_samples[col].iloc[nnarray[nn]] - minority_samples[col].iloc[i]
-#                 gap: float = random.random()
-#                 synthetic[self.newindex, col_index] = minority_samples[col].iloc[i] + gap*dif
-#             self.newindex += 1
+def populate(nnarray, i, k, N, minority_samples: pd.DataFrame) -> np.ndarray:
+    """
+    Generates a sample
+    """
+    synthetic: np.ndarray = np.empty((N, len(minority_samples.columns)))
+    for newindex in range(N):
+        nn: int = random.randint(0, k-1)
+        for col_index, col in enumerate(minority_samples.columns):
+            dif: float = minority_samples[col].iloc[nnarray[nn]] - minority_samples[col].iloc[i]
+            gap: float = random.random()
+            synthetic[newindex, col_index] = minority_samples[col].iloc[i] + gap*dif
+    return synthetic
 
 T = TypeVar('T', bound=pd.DataFrame|dd.DataFrame)
 def reassign_xss(data: T) -> T:
@@ -77,7 +100,7 @@ def reassign_xss(data: T) -> T:
         data.loc[data['Label'] == 'Brute Force -XSS', 'attack category'] = 'Brute Force'
     return data
 
-def preprocess(data: pd.DataFrame, target: str='attack category') -> tuple[pd.DataFrame, pd.DataFrame, np.ndarray, np.ndarray]:
+def preprocess(data: pd.DataFrame, target: str='attack category', smote_amounts:dict[str,int]|None=None) -> tuple[pd.DataFrame, pd.DataFrame, np.ndarray, np.ndarray]:
     data = data.drop(columns=['Src IP', 'Dst IP', 'Flow ID'])  # remove string columns
     data = data.fillna(0)  # change NaN values to 0
 
@@ -90,8 +113,8 @@ def preprocess(data: pd.DataFrame, target: str='attack category') -> tuple[pd.Da
     data['Time'] = data['Timestamp'].dt.strftime('%H%M%S').astype(int)
     data = data.drop(columns=['Timestamp'])
 
-    data['Init Fwd Win Byts Neg'] = data['Init Fwd Win Byts'] < 0  # add Negative columns
-    data['Init Bwd Win Byts Neg'] = data['Init Bwd Win Byts'] < 0
+    data['Init Fwd Win Byts Neg'] = (data['Init Fwd Win Byts'] < 0).astype('int8')  # add Negative columns
+    data['Init Bwd Win Byts Neg'] = (data['Init Bwd Win Byts'] < 0).astype('int8')
     
     label_encoding = {'Benign': '0', 'Bot': '1', 'Brute Force': '2', 'DoS': '3', 'Infiltration': '4', 'Injection': '5'}
     data[target] = data[target].replace(label_encoding).astype('int8')  # encode labels
@@ -101,6 +124,9 @@ def preprocess(data: pd.DataFrame, target: str='attack category') -> tuple[pd.Da
     X = data.drop(columns=['Label', 'attack name', 'attack category'])  # remove all label columns
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=True)  # shuffle and split
-    # TODO: SMOTE
+    print('Applying SMOTE...')
+    if smote_amounts is not None:
+        smote = SMOTE(5)
+        X_train, y_train = smote.fit_resample(X_train, y_train, amounts={int(label_encoding[k]):v for k, v in smote_amounts.items()})
 
     return X_train, X_test, y_train, y_test
--- src/replication/supervised.py
+++ src/replication/supervised.py
@@ -1,7 +1,7 @@
 from dask import dataframe as dd
 import pandas as pd
 import numpy as np
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, accuracy_score
 
 from data.loading import CIC_IDS_2018, load_cic_ids_2018
 from preprocessing.supervised import preprocess, reassign_xss
@@ -22,14 +22,14 @@ if __name__ == '__main__':
     sampling_strategy = {'Benign': 2856035, 'Bot': 286191, 'Brute Force': 513, 'DoS': 1289544, 'Infiltration': 93063, 'Injection': 53}
     samples: list[dd.DataFrame] = []
     for label, count in sampling_strategy.items():
-        label_samples = dataset.data.loc[dataset.data['attack category'] == label].sample(frac=count/value_counts[label])
+        label_samples = dataset.data.loc[dataset.data['attack category'] == label].sample(frac=count/value_counts[label], random_state=342793284)
         samples.append(label_samples)
     
-    data: pd.DataFrame = dd.concat(samples).sample(frac=1).compute()  # shuffle and compute
+    data: pd.DataFrame = dd.concat(samples).sample(frac=1, random_state=342793284).compute()  # shuffle and compute
     print(data['attack category'].value_counts())
 
     print('Preprocessing...')
-    X_train, X_test, y_train, y_test = preprocess(data)
+    X_train, X_test, y_train, y_test = preprocess(data, smote_amounts={'Brute Force': 286191-513, 'Infiltration': 286191-93063, 'Injection': 286191-53})
     print(np.unique(y_train, return_counts=True)[1])
     print(np.unique(y_test, return_counts=True)[1])
     algorithms = [random_forest, decision_tree, ada_boost, gradient_bosting, k_neighbours, linear_discriminant_analysis]
@@ -41,4 +41,12 @@ if __name__ == '__main__':
         y_pred = model.predict(X_test)
         print('Results of', algorithm_function.__name__, ':')
         report = classification_report(y_test, y_pred)
+        # Calculate per-class accuracy
+        accs = {}
+        for level in np.unique(y_test):
+            mask = y_test == level
+            acc = accuracy_score(y_test[mask], y_pred[mask])
+            accs[level] = acc
+        print(pd.Series(accs))
         print(report)
+        print('NEXT!')
