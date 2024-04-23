60aa2c6fa13a7c3fb5605c07fa3262fc45e63e6c
Calvin <calvin.azzopardi.21@um.edu.mt>
2024-04-23 18:08:01 +0200

    [replication] fix: updated replication script to match changes

--- src/replication/supervised.py
+++ src/replication/supervised.py
@@ -1,9 +1,11 @@
+import pickle
+
 from dask import dataframe as dd
 import pandas as pd
 import numpy as np
 from sklearn.metrics import classification_report, accuracy_score
 
-from data.loading import CIC_IDS_2018, load_cic_ids_2018
+from data.loading import load_cic_ids_2018
 from preprocessing.supervised import preprocess, reassign_xss
 from models.supervised import *
 
@@ -11,18 +13,18 @@ from models.supervised import *
 
 if __name__ == '__main__':
     print('Loading dataset...')
-    dataset: CIC_IDS_2018 = load_cic_ids_2018(n_workers=10)
+    dataset: dd.DataFrame = load_cic_ids_2018(n_workers=10)
     
     print('Reassigning XSS category...')
-    dataset.data = reassign_xss(dataset.data)
+    dataset.data = reassign_xss(dataset)
 
     # stratified sampling to produce data distrbution from the original study
     print('Sampling dataset...')
-    value_counts = dataset.data['attack category'].value_counts().compute()
+    value_counts = dataset['attack category'].value_counts().compute()
     sampling_strategy = {'Benign': 2856035, 'Bot': 286191, 'Brute Force': 513, 'DoS': 1289544, 'Infiltration': 93063, 'Injection': 53}
     samples: list[dd.DataFrame] = []
     for label, count in sampling_strategy.items():
-        label_samples = dataset.data.loc[dataset.data['attack category'] == label].sample(frac=count/value_counts[label], random_state=342793284)
+        label_samples = dataset.loc[dataset['attack category'] == label].sample(frac=count/value_counts[label], random_state=342793284)
         samples.append(label_samples)
     
     data: pd.DataFrame = dd.concat(samples).sample(frac=1, random_state=342793284).compute()  # shuffle and compute
@@ -30,8 +32,9 @@ if __name__ == '__main__':
 
     print('Preprocessing...')
     X_train, X_test, y_train, y_test = preprocess(data, smote_amounts={'Brute Force': 286191-513, 'Infiltration': 286191-93063, 'Injection': 286191-53})
-    print(np.unique(y_train, return_counts=True)[1])
-    print(np.unique(y_test, return_counts=True)[1])
+    pkl = (X_train, X_test, y_train, y_test)
+    # pickle.dump(pkl, open('temp/smoted_replication.pkl', 'wb'))
+    # X_train, X_test, y_train, y_test = pickle.load(open('temp/smoted.pkl', 'rb'))
     algorithms = [random_forest, decision_tree, ada_boost, gradient_bosting, k_neighbours, linear_discriminant_analysis]
     for algorithm_function in algorithms:
         model = algorithm_function()
@@ -49,4 +52,3 @@ if __name__ == '__main__':
             accs[level] = acc
         print(pd.Series(accs))
         print(report)
-        print('NEXT!')
