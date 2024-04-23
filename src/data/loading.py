55bd466d84cdc0bd77a73fa2dedb895ea06d0db3
Calvin <calvin.azzopardi.21@um.edu.mt>
2024-04-23 10:29:31 +0200

    [preprocessing] fix: removed cluster from loading script

--- src/data/loading.py
+++ src/data/loading.py
@@ -2,41 +2,18 @@ import os
 
 import pandas as pd
 import dask.dataframe as dd
-from distributed import Client, LocalCluster
 
 from data.utils import dtype_cic_ids2018, dtype_nsl_kdd
 
 
-class CIC_IDS_2018:
-    client: Client|None = None
-    count = 0
-
-    def __init__(self, dataset_path, n_workers=None) -> None:
-        # setup cluster
-        if CIC_IDS_2018.client is None:
-            if n_workers is None:
-                n_workers = os.cpu_count()
-            cluster = LocalCluster(n_workers=n_workers)
-            CIC_IDS_2018.client = Client(cluster)
-        CIC_IDS_2018.count += 1
-
-        self.data: dd.DataFrame = dd.read_csv(dataset_path, dtype=dtype_cic_ids2018)
-        self.data['Timestamp'] = dd.to_datetime(self.data['Timestamp'])
-
-    def __del__(self):
-        CIC_IDS_2018.count -= 1
-        if CIC_IDS_2018.count == 0:
-            assert CIC_IDS_2018.client is not None
-            CIC_IDS_2018.client.close()
-            CIC_IDS_2018.client = None
-
-
-def load_cic_ids_2018(meta_labelled=True, n_workers=None) -> CIC_IDS_2018:
+def load_cic_ids_2018(meta_labelled=True, n_workers=None) -> dd.DataFrame:
     if not meta_labelled:
         raise NotImplementedError('TODO: implement raw csv loading')
 
     dataset_path: str = os.path.join('data', 'CSE-CIC-IDS-2018', 'meta_labelled_data', '*.part')
-    return CIC_IDS_2018(dataset_path, n_workers=n_workers)    
+    data: dd.DataFrame = dd.read_csv(dataset_path, dtype=dtype_cic_ids2018)
+    data['Timestamp'] = dd.to_datetime(self.data['Timestamp'])
+    return data    
 
 def load_nsl_kdd() -> tuple[pd.DataFrame, pd.DataFrame]:
     train_path: str = os.path.join('data', 'NSL-KDD', 'KDDTrain+.txt')
