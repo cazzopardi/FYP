1e1cfa691816592859062d18bbb574f46f24f231
Calvin <calvin.azzopardi.21@um.edu.mt>
2024-04-30 00:05:33 +0200

    [models] feature: improved uns DL model efficiency

--- src/models/unsupervised_dl.py
+++ src/models/unsupervised_dl.py
@@ -1,49 +1,78 @@
+import math
+import pickle
+
 import numpy as np
 import tensorflow as tf
+from keras.src.engine import data_adapter
+from tensorflow.python.keras import backend as tf_backend
 from thundersvm import OneClassSVM
 
-class SAE_OCSVM:
-    def __init__(self, learning_rate:float, n_input: int, num_hidden_1: int, num_hidden_2: int, num_hidden_3: int, nu=0.5) -> None:
-        self.encoder = tf.keras.Sequential([
+class SAE_OCSVM(tf.keras.Model):
+    def __init__(self, learning_rate:float, n_input: int, num_hidden_1: int, num_hidden_2: int, num_hidden_3: int, nu=0.5, ocsvm_gpu=0) -> None:
+        super().__init__()
+        self.encoder: tf.keras.Sequential = tf.keras.Sequential([
             tf.keras.layers.Dense(num_hidden_1, activation='tanh'),
             tf.keras.layers.Dense(num_hidden_2, activation='tanh'),
-            tf.keras.layers.Dense(num_hidden_3, activation='tanh')
+            tf.keras.layers.Dense(num_hidden_3, activation='tanh', dtype='float32')
         ])
 
-        self.decoder = tf.keras.Sequential([
+        self.decoder: tf.keras.Sequential = tf.keras.Sequential([
             tf.keras.layers.Dense(num_hidden_2, activation='tanh'),
             tf.keras.layers.Dense(num_hidden_1, activation='tanh'),
             tf.keras.layers.Dense(n_input, activation='tanh')
         ])
 
         self.optimizer = tf.keras.optimizers.Adadelta(learning_rate)
+        self.compile(optimizer=self.optimizer, loss='mean_squared_error')
 
-        self.clf = OneClassSVM(nu=nu, kernel="rbf", gamma=0.1)
+        self.clf = OneClassSVM(nu=nu, kernel="rbf", gamma=0.1, gpu_id=ocsvm_gpu, max_mem_size=8192)
 
-    def train(self, X_train, num_steps=500, batch_size=100, display_step=10, display_callback=None):
-        num_batch = int(X_train.shape[0]/batch_size)
-        for i in range(num_steps):
-            re = 0
-            for i_batch in range(num_batch):
-                batch_x = X_train[i_batch*batch_size:(i_batch+1)*batch_size]
-                with tf.GradientTape() as tape:
-                    z_train = self.encoder(batch_x, training=True)
-                    recon_batch = self.decoder(z_train, training=True)
-                    re_batch = tf.reduce_mean(tf.square(recon_batch - batch_x))
-                    ae_loss = re_batch + 10 * tf.reduce_mean(tf.square(z_train))  # type: ignore
-                    gradients = tape.gradient(ae_loss, self.encoder.trainable_variables + self.decoder.trainable_variables)
-                    self.optimizer.apply_gradients(zip(gradients, self.encoder.trainable_variables + self.decoder.trainable_variables))
-                re += re_batch.numpy()  # type: ignore
+    def infer_similarity(self, X: tf.Tensor, batch_size:int=300) -> np.ndarray:
+        encoded_data = self.encoder.predict(X, batch_size=batch_size)
+        similarities = []
+        for i in range(math.ceil(len(X)/batch_size)):
+            start = i*batch_size
+            end = min(i*batch_size+batch_size, len(X))
+            similarities.append(self.clf.decision_function(encoded_data[start:end]))  # type: ignore
+        return np.concatenate(similarities)
+        # return self.clf.decision_function(self.encoder(X))
     
-            if i % display_step == 0 or i == 1:
-                # z_train = self.encoder(X_train, training=False)
-                # z_test = self.encoder(x_test, training=False)
-                # fpr_ae, tpr_ae, auc_ae = AUC_AE(x_test, y_test)
-                # fpr_svm, tpr_svm, auc_svm = AUC_SVM(z_train, z_test, y_test)
-                # print(f'Step {i}: Minibatch Loss: {re/num_batch:.4f} - AUC_AE {auc_ae:.3f} - AUC_SVM:{auc_svm:.3f}')
-                print(f'Step {i}: Minibatch Loss: {re/num_batch:.4f}')
-                if display_callback:
-                    display_callback(self)
-        self.clf.fit(self.encoder(X_train))
-    def predict(self, X):
-        return self.clf.decision_function(self.encoder(X))
+    # def infer_class(self, X: tf.Tensor, batch_size:int=300):
+    #     encoded_data = self.encoder.predict(X, batch_size=batch_size)
+    #     similarities = []
+    #     for i in range(math.ceil(len(X)/batch_size)):
+    #         start = i*batch_size
+    #         end = min(i*batch_size+batch_size, len(X))
+    #         similarities.append(self.clf.predict(encoded_data[start:end]))  # type: ignore
+    #     return np.concatenate(similarities)
+    
+    def train_step(self, data):
+        x, y, sample_weight = data_adapter.unpack_x_y_sample_weight(data)
+        with tf.GradientTape() as tape:
+            z_train = self.encoder(x, training=True)
+            recon_batch = self.decoder(z_train, training=True)
+            re_batch = self.compute_loss(x, y, recon_batch, sample_weight)
+            ae_loss = re_batch + tf.constant(10.0, dtype='float32') * tf.reduce_mean(tf.square(z_train))
+        self._validate_target_and_loss(y, ae_loss)
+        self.optimizer.minimize(ae_loss, self.trainable_variables, tape=tape)
+        return self.compute_metrics(x, y, recon_batch, sample_weight)
+
+    def train(self, X_train: np.ndarray|tf.Tensor, epochs=10, batch_size=100):
+        if type(X_train) == np.ndarray:
+            X_train = tf.convert_to_tensor(X_train, dtype='float32')
+        self.fit(X_train, X_train, epochs=epochs, batch_size=batch_size, verbose=1)
+        encoded_data = self.encoder.predict(X_train, batch_size=batch_size)
+        tf_backend.clear_session()
+        print('Fitting OCSVM')
+        self.save_weights('dl_model')
+        # self.load_weights('dl_model')
+        self.clf.fit(encoded_data)
+        print('Training complete.')
+    
+    def save(self, path:str):
+        self.save_weights(path)
+        self.clf.save_to_file(path+'.sk')
+
+    def load(self, path: str):
+        self.load_weights(path)
+        self.clf.load_from_file(path+'.sk')
